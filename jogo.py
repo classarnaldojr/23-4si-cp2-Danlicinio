0dfe96ab7ffa7c58a6d7e9650568f83cba1e253f
Dani Licinio <66219047+Danlicinio@users.noreply.github.com>
2023-04-27 19:36:57 -0300

    Jogo

--- jogo.py
+++ jogo.py
@@ -1,136 +1,113 @@
-#biblioteca OpenCV
 import cv2
-#framework criado pelo Google de código aberto, capax de detectar elementos em imagem e vídeo
 import mediapipe as mp
-#biblioteca Numpy
-import numpy as np
-#processamento de imagens, tranlação, rotação, redimensionamento, esqueletização e exibição de imagem 
-import imutils
-
-#utilizando mediapipe
+import random
+#MediaPipe
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
-
-#identificar se pedra, papel e tesoura
-def getHandMove(hand_landmarks):
-    landmarks = hand_landmarks.landmark
-    if (landmarks[5].y < landmarks[8].y) and (landmarks[9].y < landmarks[12].y) and (landmarks[13].y < landmarks[16].y) and (landmarks[17].y < landmarks[20].y): return "pedra"
-    elif (landmarks[5].y > landmarks[8].y) and (landmarks[9].y > landmarks[12].y) and (landmarks[13].y > landmarks[16].y) and (landmarks[17].y > landmarks[20].y): return "papel"
-    else: return "tesoura"
-
-#captura do video
-cap = cv2.VideoCapture('pedra-papel-tesoura.mp4')
-
-#gameText, string que vai mostrar quem venceu
-gameText = ""
-
-#identificar dentificar as mãos
-with mp_hands.Hands(
-    model_complexity=0, 
-    min_detection_confidence=0.5, 
-    min_tracking_confidence=0.5) as hands:
-
-    scores = [0, 0]
-
-    while True:
-        success, img = cap.read()
-
-        #achar altura e largura do video
-        h, w, _ = img.shape
-
-        #dividir a imagem do video em 2
-        half = w//2
-
-        #separa o lado esquerdo do video
-        left_part = img[:, :half]
-        #separa o lado direito do video
-        right_part = img[:, half:]
-
-        #parte esquerda girando em 90 graus 
-        left_up = imutils.rotate(left_part, angle=90)
-        #parte direita girando em 270 graus 
-        right_up = imutils.rotate(right_part, angle=270)
-
-        #juntando os lados
-        img = cv2.hconcat([left_up, right_up])
-
-        img.flags.writeable = False
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        results = hands.process(img)
-
-        #identificando as maos e desenhando os pontos
-        img.flags.writeable = True
-        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-        if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
+# Lógica para determinar o formato da mão (pedra, papel ou tesoura) com base nas coordenadas dos dedos
+def determine_hand_gesture(finger_coordinates):
+    
+    # Retorna um formato aleatório apenas para exemplo
+    return random.choice(["Pedra", "Papel", "Tesoura"])  
+def determine_winner(hand1_gesture, hand2_gesture):
+    if hand1_gesture == hand2_gesture:
+        return "Empate", None
+    if hand1_gesture == "Pedra":
+        if hand2_gesture == "Tesoura":
+            return "Mao 1", "Pedra"
+        elif hand2_gesture == "Papel":
+            return "Mao 2", "Papel"
+    if hand1_gesture == "Papel":
+        if hand2_gesture == "Pedra":
+            return "Mao 1", "Papel"
+        elif hand2_gesture == "Tesoura":
+            return "Mao 2", "Tesoura"
+    if hand1_gesture == "Tesoura":
+        if hand2_gesture == "Papel":
+            return "Mao 1", "Tesoura"
+        elif hand2_gesture == "Pedra":
+            return "Mao 2", "Pedra"
+    return None, None
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(model_complexity=0, max_num_hands=2, min_detection_confidence=0.5)
+# Captura de vídeo da webcam (altere o valor para o caminho do vídeo, se necessário)
+cap = cv2.VideoCapture('pedra-papel-tesoura.mp4')  
+# Inicialização dos placares
+score_player1 = 0
+score_player2 = 0
+while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+        break
+    image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+    image.flags.writeable = False
+    results = hands.process(image)
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    player1_gesture = None
+    player2_gesture = None
+    if results.multi_hand_landmarks:
+        for i, hand_landmarks in enumerate(results.multi_hand_landmarks):
+            finger_coordinates = []
+            for landmark in hand_landmarks.landmark:
+                x = int(landmark.x * image.shape[1])
+                y = int(landmark.y * image.shape[0])
+                finger_coordinates.append((x, y))
+            hand_gesture = determine_hand_gesture(finger_coordinates)
+            if i == 0:
+                player1_gesture = hand_gesture
+            elif i == 1:
+                player2_gesture = hand_gesture
+            if hand_gesture:
                 mp_drawing.draw_landmarks(
-                    img,
+                    image,
                     hand_landmarks,
                     mp_hands.HAND_CONNECTIONS,
-                    mp_drawing_styles.get_default_hand_landmarks_style(),
-                    mp_drawing_styles.get_default_hand_connections_style()
-                )
-
-        hls = results.multi_hand_landmarks
-        #se dentro dos resuldados tiver 2 maos
-        if hls and len(hls) == 2:
-            #pegar o gesto da mao da direita
-            p2_move = getHandMove(hls[0])
-            #pegar o gesto da mao da esquerda
-            p1_move = getHandMove(hls[1])
-            #condicoes para quem ganha
-            if success:
-                if p1_move == p2_move: gameText = "Empate"
-                elif p1_move == "papel" and p2_move == "pedra": 
-                    gameText = "Jogador 1 vence"
-                elif p1_move == "papel" and p2_move == "tesoura": 
-                    gameText = "Jogador 2 vence"
-                elif p1_move == "pedra" and p2_move == "tesoura": 
-                    gameText = "Jogador 1 vence"
-                elif p1_move == "pedra" and p2_move == "papel": 
-                    gameText = "Jogador 2 vence"
-                elif p1_move == "tesoura" and p2_move == "papel": 
-                    gameText = "Jogador 1 vence"
-                elif p1_move == "tesoura" and p2_move == "pedra": 
-                    gameText = "Jogador 2 vence"
-                else:
-                    print("Not identified")
-            else:
-                success = False
-
-            if gameText == "Jogador 1 vence":
-                scores[0] += 1
-            else:
-                scores[1] += 1
-
-        #voltando o video para a posicao original
-        #separa o lado esquerdo do video
-        left_part = img[:, :half]
-        #separa o lado direito do video
-        right_part = img[:, half:]
-
-        #parte esquerda girando em 270 graus 
-        left_up = imutils.rotate(left_part, angle=270)
-        #parte esquerda girando em 90 graus 
-        right_up = imutils.rotate(right_part, angle=90)
-        
-        #juntando os lados
-        img = cv2.hconcat([left_up, right_up])
-
-        #add textos
-        cv2.putText(img, gameText, (600, 950), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,0,0), 2)
-        cv2.putText(img, str("Jogador 1"), (100, 200), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,0,0), 2)
-        cv2.putText(img, p1_move, (100, 300), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,0,0), 2)
-        cv2.putText(img, str(scores[0]), (100, 400), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,0,0), 2)
-        cv2.putText(img, str('Jogador 2'), (1400, 200), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,0,0), 2)
-        cv2.putText(img, p2_move, (1400, 300), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,0,0), 2)
-        cv2.putText(img, str(scores[1]), (1400, 400), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,0,0), 2)
-
-        cv2.imshow('Hands', img)
-
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            break
-
+                    mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=2, circle_radius=2
+                ),
+                mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=2),
+            )
+            cv2.putText(
+                image,
+                hand_gesture,
+                (int(hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].x * image.shape[1]),
+                 int(hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].y * image.shape[0])),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1,
+                (0, 0, 255),
+                2,
+                cv2.LINE_AA,
+            )
+    winner, winning_gesture = determine_winner(player1_gesture, player2_gesture)
+    if winner:
+        cv2.putText(
+            image,
+            "Vencedor: {} - {}".format(winner, winning_gesture),
+            (20, 50),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0, 255, 0),
+            2,
+            cv2.LINE_AA,
+        )
+        if winner == "Mao 1":
+            score_player1 += 1
+        else:
+            score_player2 += 1
+    # Exibir o placar dos jogadores
+    cv2.putText(
+        image,
+        "Placar - Mao 1: {}  Mao 2: {}".format(score_player1, score_player2),
+        (20, 80),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (0, 255, 0),
+        2,
+        cv2.LINE_AA,
+    )
+    cv2.imshow("Pedra-Papel-Tesoura", image)
+    if cv2.waitKey(5) & 0xFF == ord('q'):
+        break
 cap.release()
 cv2.destroyAllWindows()
